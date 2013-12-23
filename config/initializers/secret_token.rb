d2719057eee8702d90a01d48896596004c8c38cc
Giovanni Wong <deargiovanni@hotmail.com>
2013-12-23 15:54:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inyourshoes::Application.config.secret_key_base = '8dd3e4a8e1e8f94c1d10ac281d022e6b8e2ee78416cd31e2f89cf59d40c4218d994020428a93edc22f680bf170f9053526435fbf9c13e7e66904feda9b08915a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inyourshoes::Application.config.session_store :cookie_store, key: '_inyourshoes_session'
